3152bf4970b51480f96cf7c82a553ed1b929d7b3
Ренат <renat1703@gmail.com>
2023-03-02 19:59:25 +0300

    Add GitHubTests

---
+++ src/test/java/GitHubTests.java
@@ -0,0 +1,28 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GitHubTests {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void textTest()
+    {
+        open("https://github.com/");
+
+        $("[placeholder ='Search GitHub']").setValue("Selenide").pressEnter();
+        $$("ul.repo-list").first().$("a").click();
+        $("#wiki-tab").click();
+        $("li.wiki-more-pages-link").$("[type=button]").click();
+        $(byText("SoftAssertions")).click();
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class:"));
+        sleep(5000);
+    }
+}
